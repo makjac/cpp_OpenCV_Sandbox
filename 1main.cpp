a17cf92295eb26eb250312fdddd245c6ea0492b2
Maks Jackowski <makjac@st.amu.edu.pl>
2021-11-05 23:45:52 +0100

    _

--- 1main.cpp
+++ 1main.cpp
@@ -8,8 +8,8 @@ int main()
     image=cv::imread("images/test.png");
 	cv::circle(image, cv::Point(100,100), 40, cv::Scalar(43, 105, 63), cv::FILLED);
 	cv::Mat imageCanny, gaussinImg;
-	cv::GaussianBlur(image, gaussinImg, cv::Size(3,3), 100, 5);
-	cv::Canny(gaussinImg, imageCanny, 25,200);
+	cv::GaussianBlur(image, gaussinImg, cv::Size(3,3), 100, 50);
+	cv::Canny(gaussinImg, imageCanny, 2,300);
 	cv::putText(image, "INPUT", cv::Point(10, 20), cv::FONT_HERSHEY_DUPLEX, 0.7, cv::Scalar(255, 101, 197), 2);
 	cv::putText(imageCanny, "CANNY", cv::Point(10, 20), cv::FONT_HERSHEY_DUPLEX, 0.7, cv::Scalar(255, 101, 197), 2);
 	cv::putText(gaussinImg, "GAUSS", cv::Point(10, 20), cv::FONT_HERSHEY_DUPLEX, 0.7, cv::Scalar(255, 101, 197), 2);
